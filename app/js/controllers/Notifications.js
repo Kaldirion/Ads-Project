aede752aac37a93d9f5b749a7501e3c42cd4ea5e
Kaldirion <kaldirion@gmail.com>
2015-01-11 09:49:35 +0200

    alert issue resolved

--- app/js/controllers/Notifications.js
+++ app/js/controllers/Notifications.js
@@ -1,10 +1,10 @@
 "use strict";
 adsApp.factory('notification', [ function () {
     function errorMessage(message) {
-        $('#notes').notify().errorMessage(message);
+        $('#alerts').notify().errorMessage(message);
     }
     function successMessage(message) {
-        $('#notes').notify().successMessage(message);
+        $('#alerts').notify().successMessage(message);
     }
     return {
         errorMessage: errorMessage,
