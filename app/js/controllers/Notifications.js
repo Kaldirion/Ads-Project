c7796874ebf0de3b335b0f65e16102d4dcefdf23
Kaldirion <kaldirion@gmail.com>
2015-01-11 10:47:06 +0200

    notification adjusted

--- app/js/controllers/Notifications.js
+++ app/js/controllers/Notifications.js
@@ -1,10 +1,20 @@
 "use strict";
 adsApp.factory('notification', [ function () {
-    function errorMessage(message) {
-        $('#alerts').notify().errorMessage(message);
+
+    function errorMessage(Msg){
+        $('#alerts').noty({
+            text: Msg,
+            type: "error",
+            timeout: 5000
+        });
     }
-    function successMessage(message) {
-        $('#alerts').notify().successMessage(message);
+
+    function successMessage(Msg) {
+        $('#alerts').noty({
+            text: Msg,
+            type: "success",
+            timeout: 5000
+        });
     }
     return {
         errorMessage: errorMessage,
